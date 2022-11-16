bf891f9f8475ed9290dc12b582f8f1cfcdccb333
Dmitrij Sudnitsyn <dmitrij.sudnitsyn@gmail.com>
2022-11-16 22:11:30 +0200

    Класс Circle шаг 3

--- ClassLesson.py
+++ ClassLesson.py
@@ -8,19 +8,34 @@
 # ■■Пропорциональное изменение размеров окружности,
 # путем изменения ее радиуса (операции + - += -=).
 class Circle:
+   def is_number(self,str):
+      try:
+       float(str)  
+       return True
+      except ValueError:
+       print("Переменная {str} не численного типа")  
+       return False
+
    def __init__(self,r):
-      self.r=r
-      self.l=2*3.14*r
+      if self.is_number(r):
+       self.r=r
+       self.l=2*3.14*r
+      else:
+       print(f"переменная {r} не числового типа")   
 
    def set(self,r):
-       self.r = r
+      if self.is_number(r):
+       self.r=r
        self.l=2*3.14*r
+      else:
+       print(f"переменная {r} не числового типа")   
 
    def setR(self,r):
+      if self.is_number(r):      
        self.r = r
-
-   def setL(self,r):
-       self.l =2*3.14*r
+       self.l =2*3.14*r       
+      else:
+       print(f"переменная {r} не числового типа")   
 
    def getR(self):
       return self.r
@@ -31,18 +46,20 @@ class Circle:
    def __str__(self):
      return f'Радиус окружности = {self.r} Длина окружности {2*3.14*self.r}\n'
 
-# __lt__(self, other) - x < y вызывает x.__lt__(y).
-   def __lt__(self,other):
-     return 2*3.14*self.r<=2*3.14*other.r      
-
-# __le__(self, other) - x ≤ y вызывает x.__le__(y).
-   def __le__(self,other):
-     return 2*3.14*self.r<=2*3.14*other.r
-
 # Проверка на равенство радиусов двух окружностей (операция = =);
 # __eq__(self, other) - x == y вызывает x.__eq__(y).
    def __eq__(self, other):
       return self.r==other.r
+
+# Сравнения длин двух окружностей (операции >, <,<=,>=);
+
+# __lt__(self, other) - x < y вызывает x.__lt__(y).
+   def __lt__(self,other):
+     return self.l<=other.l
+
+# __le__(self, other) - x ≤ y вызывает x.__le__(y).
+   def __le__(self,other):
+     return self.l<=other.l
 
 # __ne__(self, other) - x != y вызывает x.__ne__(y)
    def __ne__(self,other):
@@ -55,4 +72,41 @@ class Circle:
 # __ge__(self, other) - x ≥ y вызывает x.__ge__(y).      
    def __ge__(self,other):
      return self.l>=other.l      
+
+
+# Пропорциональное изменение размеров окружности, путем изменения ее радиуса (операции + - += -=).
+
+
+# __add__(self, other) - сложение. x + y вызывает x.__add__(y).
+   def __add__(self,a):
+      try:
+       return Circle(self.r+a)
+      except ValueError:
+       print(f"  {a} тип значения не соответсвует типу операции ")     
+
+# __iadd__(self, other) - +=.
+   def __iadd__(self,a):
+      try:
+       self.r+=a  
+       return self
+      except TypeError:
+       print(f"  {a} тип значения не соответсвует типу операции ")     
+
+       
+      
+# __sub__(self, other) - вычитание (x - y).
+   def __sub__(self,a):
+      try:
+       return Circle(self.r-a)
+      except TypeError:
+       print(f"  {a} тип значения не соответсвует типу операции ")     
+
+
+# __isub__(self, other) - -=.
+   def __isub__(self,a):
+      try:
+       self.r-=a  
+       return self
+      except TypeError:
+       print(f"  {a} тип значения не соответсвует типу операции ")     
 
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from ClassLesson import *
 
-myCircle =Circle(5)
+myCircle =Circle("dsd")
 print(myCircle)
-myCircle.set(15)
-myCircle2=Circle(30)
-if myCircle<myCircle2:
-   print(f"Длина окружности {myCircle.getL()} меньше длины окружности {myCircle2.getL()}") 
-myCircle.set(30)   
-if myCircle==myCircle2:
-   print(f"Длина окружности {myCircle.getL()} равна длины окружности {myCircle2.getL()}") 
-
+# myCircle.set(15)
+# myCircle2=Circle(30)
+# # if myCircle<myCircle2:
+# #    print(f"Длина окружности {myCircle.getL()} меньше длины окружности {myCircle2.getL()}") 
+# # myCircle.set(30)   
+# # if myCircle==myCircle2:
+# #    print(f"Длина окружности {myCircle.getL()} равна длины окружности {myCircle2.getL()}") 
+# print(myCircle)
+# myCircle=myCircle+10
+# print(myCircle)
+# myCircle+=10
+# print(myCircle)
 
