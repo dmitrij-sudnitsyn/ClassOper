c57bc3735f038367bb005f9d86d1c6c7cc94abdd
Dmitrij Sudnitsyn <dmitrij.sudnitsyn@gmail.com>
2022-11-17 20:52:14 +0200

    Формирования класса Airplain

--- ClassLesson.py
+++ ClassLesson.py
@@ -7,8 +7,7 @@
 # Длина окружности l=2πr
 # ■■Пропорциональное изменение размеров окружности,
 # путем изменения ее радиуса (операции + - += -=).
-class Circle:
-   def is_number(self,str):
+def is_number(self,str):
       try:
        float(str)  
        return True
@@ -16,22 +15,24 @@ class Circle:
        print("Переменная {str} не численного типа")  
        return False
 
+
+class Circle:
    def __init__(self,r):
-      if self.is_number(r):
+      if is_number(r):
        self.r=r
        self.l=2*3.14*r
       else:
        print(f"переменная {r} не числового типа")   
 
    def set(self,r):
-      if self.is_number(r):
+      if is_number(r):
        self.r=r
        self.l=2*3.14*r
       else:
        print(f"переменная {r} не числового типа")   
 
    def setR(self,r):
-      if self.is_number(r):      
+      if is_number(r):      
        self.r = r
        self.l =2*3.14*r       
       else:
@@ -115,4 +116,95 @@ class Circle:
         return self
       except ValueError:
        print(f"  {a} тип значения не соответсвует типу операции ")     
+
+# Вам необходимо создать класс Airplane (самолет). С помощью перегрузки операторов реализовать:
+# ■■Проверка на равенство типов самолетов (операция = =);
+# ■■Увеличение и уменьшение пассажиров в салоне самолета (операции + - += -=);
+# ■■Сравнение двух самолетов по максимально возможному количеству пассажиров на борту (операции > < <= >=).
+
+class Airplane:
+  def __init__(self,TypeAirplane,MaxKolPass,KolPass) -> None:
+    self.TypeAirplane =TypeAirplane
+    self.MaxKolPass      =MaxKolPass
+    self.KolPass      =KolPass
+
+
+  def __str__(self) -> str:
+    return f'Самолет типа {self.TypeAirplane} количество пассажирова {self.KolPass} при максимально возможном количестве пассажиров {self.MaxKolPass}'
+
+  def set(self,TypeAirplane,KolPass):
+    self.TypeAirplane = TypeAirplane
+    self.MaxKolPass   = MaxKolPass
+    self.KolPass      = KolPass
+  
+  def getTypeAirplane(self):
+   return self.TypeAirplane
+
+  def getMaxKolPass(self):
+   return self.MaxKolPass
+
+  def getKolPass(self):
+   return self.KolPass
+
+  def setTypeAirplane(self,a):
+    self.TypeAirplane=a
+
+  def setMaxKolPass(self,a):
+   if is_number(a):
+    if a>=0:  
+     self.MaxKolPass  =a
+    else:
+     print("Количество не может быть меньше 0. Неможет быть отрицательным")  
+   else:
+    print("Количество должно быть целочисленое")   
+
+  def setKolPass(self,a):
+   if is_number(a):
+    if a>=0:  
+     if self.MaxKolPass>=a: 
+      self.KolPass  =a
+     else:
+      print(f"Количество пассажиров не может быть больше максимально возможного количества пассажиров {self.MaxKolPass}") 
+    else:
+     print("Количество не может быть меньше 0. Неможет быть отрицательным")  
+   else:
+    print("Количество должно быть целочисленое")   
+  
+  def is_MaxKolPass(self,a):
+    if is_number(a):
+     if self.MaxKolPass>=self.KolPass+a:
+      return True
+     else:
+      print(f"Количество пассажиров не может быть больше максимально возможного количества пассажиров {self.MaxKolPass}")
+      return False  
+
+# Проверка на равенство типов самолетов (операция = =);
+# Тут проверить как сравниваются типы в Python
+  # __eq__(self, other) - x == y вызывает x.__eq__(y).
+  def __eq__(self,other) -> bool:
+   return self.TypeAirplane==other.TypeAirplane
+
+# Увеличение и уменьшение пассажиров в салоне самолета (операции + - += -=);   
+# __add__(self, other) - сложение. x + y вызывает x.__add__(y).
+  def __add__(self,a):
+    if self.is_MaxKolPass(a):
+     self.KolPass=self.KolPass+a
+     return Airplane(self)
+# __iadd__(self, other) - +=.
+  def __iadd__(self,a):
+   if self.is_MaxKolPass(a):
+      self.KolPass+=a  
+      return self
+
+# __sub__(self, other) - вычитание (x - y).
+  def __sub__(self,a):
+    if self.is_MaxKolPass(a):
+       self.KolPass=self.KolPass-a
+       return Airplane(self)
+
+# __isub__(self, other) - -=.
+  def __isub__(self,a):
+    if self.is_MaxKolPass(a):
+       self.KolPass-=a  
+       return self
 
